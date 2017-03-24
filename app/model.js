b4553e36e3a734423aca88f7ba0a7499164ec6ca
sathishvinayk <sathishvinayk@gmail.com>
2017-03-24 23:59:12 +0530

    Added model and components

---
+++ app/model.js
@@ -0,0 +1,21 @@
+"use strict";
+var Model = (function () {
+    function Model() {
+        this.user = "Adam",
+            this.items = [
+                new TodoItem('Buy Flowers', false),
+                new TodoItem('Get Shoes', false),
+                new TodoItem('Collect Tickets', true),
+                new TodoItem('Call Joe', false)];
+    }
+    return Model;
+}());
+exports.Model = Model;
+var TodoItem = (function () {
+    function TodoItem(action, done) {
+        this.action = action;
+        this.done = done;
+    }
+    return TodoItem;
+}());
+exports.TodoItem = TodoItem;
